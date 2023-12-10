1e8031cab7f5145109f1c4b5161c8d148d3d81c9
adisingh925 <adisingh925@gmail.com>
2023-12-10 18:35:48 +0530

    scaling fix

--- app/src/main/java/app/adreal/android/widgets/ClockFunctions.kt
+++ app/src/main/java/app/adreal/android/widgets/ClockFunctions.kt
@@ -10,6 +10,7 @@ import java.time.LocalDate
 import java.time.temporal.IsoFields
 import java.util.Calendar
 
+
 object ClockFunctions {
 
     @RequiresApi(Build.VERSION_CODES.O)
@@ -67,22 +68,12 @@ object ClockFunctions {
 
         Log.d("MyAppWidgetProvider", "getRemoteViews: $rows x $columns")
 
-        return if ((rows == 3 && columns >= 3)) {
-            RemoteViews(context.packageName, R.layout.widget_layout)
-        } else if (rows >= 3 && columns == 3) {
-            RemoteViews(context.packageName, R.layout.widget_layout)
-        } else if ((rows == 4 && columns >= 4)) {
-            RemoteViews(context.packageName, R.layout.large_widget_layout)
-        } else if (rows >= 4 && columns == 4) {
-            RemoteViews(context.packageName, R.layout.large_widget_layout)
-        } else if ((rows == 5 && columns >= 5)) {
-            RemoteViews(context.packageName, R.layout.extra_large_widget_layout)
-        } else if (rows >= 5 && columns == 5) {
-            RemoteViews(context.packageName, R.layout.extra_large_widget_layout)
-        } else if ((rows == 6 && columns >= 6)) {
+        return if (rows >= 6 && columns >= 6) {
             RemoteViews(context.packageName, R.layout.extra_extra_large_widget_layout)
-        } else if (rows >= 6 && columns == 6) {
-            RemoteViews(context.packageName, R.layout.extra_extra_large_widget_layout)
+        } else if (rows >= 5 && columns >= 5) {
+            RemoteViews(context.packageName, R.layout.extra_large_widget_layout)
+        } else if (rows >= 4 && columns >= 4) {
+            RemoteViews(context.packageName, R.layout.large_widget_layout)
         } else {
             RemoteViews(context.packageName, R.layout.widget_layout)
         }
